7ca7687da932a6d869d4fa39296e1f81a0dec253
Hosain <cryptichasan925@gmail.com>
2025-01-17 00:16:35 +0600

    auth interface done

---
+++ src/app/modules/Auth/auth.interface.ts
@@ -0,0 +1,44 @@
+import { Model, Types } from 'mongoose'
+import { USER_ROLE } from './auth.constance'
+
+export interface IUser {
+  _id: Types.ObjectId
+  id?: string
+  name: string
+  email: string
+  password: string
+  phone: string
+  role: 'admin'
+  profilePicture: string
+  isDeleted?: boolean
+  isBlocked?: boolean
+  needsPasswordChange?: boolean
+  passwordChangedAt?: Date
+}
+
+export interface ILoginUser {
+  email: string
+  password: string
+}
+
+export type IUserRole = keyof typeof USER_ROLE
+
+export type INewUser = {
+  password: string
+  role: string
+  email: string
+}
+
+export interface UserModel extends Model<IUser> {
+  isUserExistsByEmail(email: string): Promise<IUser>
+
+  isUserPasswordMatch(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>
+
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean
+}
