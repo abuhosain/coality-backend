662a25b62559df9c979f39982c279bae629722fb
Hosain <cryptichasan925@gmail.com>
2025-01-16 21:39:05 +0600

    coality initial setup done

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cockieParser from "cookie-parser";
+import path from "path";
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cockieParser());
+
+// cors
+const allowedOrigins = ["http://localhost:3000"]; // Production frontend
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Test route
+app.get("/", async (req: Request, res: Response) => {
+  const message = "Coality server is running";
+  res.send(message);
+});
+
+// Catch-all route for client-side routing
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+export default app;
