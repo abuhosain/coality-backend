055c575cbe852af45db1f6bfde9af323e5eb6480
Hosain <cryptichasan925@gmail.com>
2025-01-22 20:22:59 +0600

    team validation done

---
+++ src/app/modules/Team/team.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+const teamValidation = z.object({
+  body: z.object({
+    name: z
+      .string()
+      .min(1, 'Name is required')
+      .max(50, { message: 'Name cannot be more than 50 characters' })
+      .trim(),
+    description: z.string().min(1, 'Description is required').trim(),
+  }),
+});
+const updateTeamValidation = z.object({
+  body: z.object({
+    name: z.string().trim().optional(),
+    description: z.string().trim().optional(),
+  }),
+});
+
+export const TeamValidation = {
+  teamValidation,
+  updateTeamValidation,
+};
