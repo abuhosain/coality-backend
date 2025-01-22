4423cf518d78b32801d19ad1c52c37eb2025538b
Hosain <cryptichasan925@gmail.com>
2025-01-22 22:05:20 +0600

    create team done

--- src/app/modules/Team/team.routes.ts
+++ src/app/modules/Team/team.routes.ts
@@ -1,5 +1,29 @@
 import express, { NextFunction, Request, Response } from 'express';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../Auth/auth.constance';
+import { multerUpload } from '../../config/multer.config';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import validateRequest from '../../middleware/validateRequest';
+import { TeamValidation } from './team.validation';
+import { TeamController } from './team.controller';
 
 const router = express.Router();
+
+// create team
+router.post(
+  '/create-team',
+  auth(USER_ROLE.admin),
+  multerUpload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.file) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'No file uploaded');
+    }
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
+  validateRequest(TeamValidation.teamValidation),
+  TeamController.createTeam,
+);
 
 export const TeamRoutes = router;
